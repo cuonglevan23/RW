5473a27e1933a8fb2fea8c3918cbbb81381624ac
Lvc <lvc@Lvcs-MacBook-Pro.local>
2023-06-08 20:46:40 +0700

    Layout và Route

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { HeaderOnly } from '~/components/Layout';
+// Page
+import Home from '~/pages/Home';
+import Login from '~/pages/Login';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+// public routes
+const publicRoutes = [
+    { path: '/', components: Home },
+    { path: '/login', components: Login, layout: HeaderOnly },
+    { path: '/profile', components: Profile },
+    { path: '/search', components: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
